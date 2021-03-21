dd9d843c1b1ec5f95de8c0e320bfefbad5d82c82
oopanpan <oopanpan@gmail.com>
2021-03-20 22:57:18 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+ranting = () => {
+  alert('This lab is dumb')
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.ranting} />
   </div>,
   document.getElementById('root')
 );

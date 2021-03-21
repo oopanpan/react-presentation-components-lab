3e9ca1801053e77b23c919e8fbde2dd144a91464
oopanpan <oopanpan@gmail.com>
2021-03-20 22:57:05 -0700

    commit tree d84d6017637b239989f3e95b24b059f103b9f639

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+export default function SimplerComponent({ handleClick }) {
+    return (
+        <div onClick={handleClick}>
+            I am just happy
+        </div>
+    )
+}
